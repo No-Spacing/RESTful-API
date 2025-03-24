5392b0e074b79e663b0bd7ec1a7eda34530dadbb
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-24 20:54:11 +0800

    debugbar remove from the code

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Data;
 use Illuminate\Support\Facades\DB;
-use \Debugbar;
 use \Validator;
 
 class DataController extends Controller
--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
@@ -184,7 +183,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-    ])->toArray(),
-    'Debugbar' => 'BarryvdhDebugbarFacade',
-
+    ])->toArray()
+    
 ];
